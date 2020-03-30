4929c0207ecd17bb3cb9bfb09d1c4ef2d592e45a
Unknown <joshuabarua9@hotmail.com>
2020-03-30 10:50:46 +0100

    Pushing files to start version control

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-root 'application#hello'
+  get 'user/new'
+  root 'static_pages#home'
+  get 'home_path',     to:'static_pages#home'
+  get  'help_path',    to: 'static_pages#help'
+  get  'about_path',   to: 'static_pages#about'
+  get  'contact_path', to: 'static_pages#contact'
 end
