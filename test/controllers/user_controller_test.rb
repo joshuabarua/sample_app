2a26ad36f9d5d8b3a905c5d4e6f0612e36edb97c
Unknown <joshuabarua9@hotmail.com>
2020-03-31 11:29:03 +0100

    Updated/fixed tests

--- test/controllers/user_controller_test.rb
+++ test/controllers/user_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UserControllerTest < ActionDispatch::IntegrationTest
   
   test "should get new" do
-    get signup_path
+    get '/signup_path'
     assert_response :success
   end
 
