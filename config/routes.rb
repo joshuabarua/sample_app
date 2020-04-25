843ab8b3a862b8c0571498423419474e8f9a4753
Josh <joshuabarua9@hotmail.com>
2020-04-25 14:57:52 +0100

    Added Test Case

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get  '/home',     to: 'static_pages#home'
-  get  '/help',     to: 'static_pages#help'
-  get  '/about',    to: 'static_pages#about'
-  get  '/contact',  to: 'static_pages#contact'
+  get  '/home',     to: 'static_pages#home', as: 'home'
+  get  '/help',     to: 'static_pages#help', as: 'help'
+  get  '/about',    to: 'static_pages#about', as: 'about'
+  get  '/contact',  to: 'static_pages#contact',  as: 'contact'
   get  '/signup',   to: 'users#new'
-  post '/signup',  to: 'users#create'
+  post '/signup',   to: 'users#create'
   resources :users
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  
+
   def setup
     @user = User.new(name: "Example User", email: "user@example.com",
                      password: "foobar", password_confirmation: "foobar")
@@ -25,7 +25,7 @@ class UserTest < ActiveSupport::TestCase
     @user.name = "a" * 51
     assert_not @user.valid?
   end
-  
+
   test "email should not be too long" do
     @user.email = "a" * 244 + "@example.com"
     assert_not @user.valid?
@@ -63,5 +63,12 @@ class UserTest < ActiveSupport::TestCase
   test "password should have a minimum length" do
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
+  end
+
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
   end
 end
