f6c8dd5a26d7e9adfba4f8d639d516cdc0efea78
Josh <joshuabarua9@hotmail.com>
2020-04-29 14:43:44 +0100

    Tute 7 Production, Hash Errors(BCrypt)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,4 +11,19 @@ class ActiveSupport::TestCase
   def is_logged_in?
     !session[:user_id].nil?
   end
+
+  #log in as specific user
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
 end
+
+class ActionDispatch::IntegrationTest
+  #login as specific user
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                          password: 'password123',
+                                          remember_me: remember_me } }
+  end
+end
+
