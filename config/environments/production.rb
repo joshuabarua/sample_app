5e4518bc20ff4893ef579ec463661fd562619455
Joshua Barua <joshua.k.barua@gmail.com>
2020-05-28 02:05:12 +0100

    changed prod.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'rmit-sampleapp-s3610346-rad.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.default_url_options = { host: 'http://rmit-sampleapp-s3610346-rad.herokuapp.com' }
   ActionMailer::Base.smtp_settings = {
     :address=> 'smtp.sendgrid.net',
     :port => '587',
